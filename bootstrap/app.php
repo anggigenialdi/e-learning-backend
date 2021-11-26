1fa6f9b0826feb294f8719ec639bd3aa9c288bfe
unknown <tgantara@gmail.com>
2021-11-26 13:19:53 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -71,10 +71,11 @@ $app->configure('app');
 | route or middleware that'll be assigned to some specific routes.
 |
 */
+$app->middleware([
+    // App\Http\Middleware\ExampleMiddleware::class
+App\Http\Middleware\CorsMiddleware::class 
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
