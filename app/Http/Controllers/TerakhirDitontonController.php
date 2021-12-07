feab4a66e8ebf5ac743eaa7a45f35e1469b69ef3
Anggi Genialdi <anggigenialdi@gmail.com>
2021-12-07 22:59:45 +0700

    terkahir tonton

--- app/Http/Controllers/TerakhirDitontonController.php
+++ app/Http/Controllers/TerakhirDitontonController.php
@@ -37,8 +37,18 @@ class TerakhirDitontonController extends Controller
             $terakhirDitonton->kursus_id = $request->kursus_id;
             $terakhirDitonton->kelas_id = $request->kelas_id;
             $terakhirDitonton->materi_id = $request->materi_id;
-            
-            $terakhirDitonton->save();
+
+            $duplicate_data = $terakhirDitonton->where( 'kursus_id', $terakhirDitonton->kursus_id)->where('user_id',$terakhirDitonton->user_id)->where( 'kelas_id', $terakhirDitonton->kelas_id)->where( 'materi_id', $terakhirDitonton->materi_id)->first();
+            if ( $duplicate_data ) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Duplicate data',
+                    'data' => $terakhirDitonton,
+
+                ], 425);
+            } else {
+                $terakhirDitonton->save();
+            }
 
 
             return response()->json([
@@ -96,14 +106,45 @@ class TerakhirDitontonController extends Controller
 
         $history = Terakhir_ditonton::where('user_id', $idUser)->where('kursus_id', $idKursus)->first();
                 
+        $user = User::where('id', $idUser)->get();
+        $kursus = Kursus::where('id', $idKursus)->get();
+        $kelas = Kelas::where('kursus_id', $idKursus)->get();
 
         if($history){
+            $data = [];
+            $data_kursus = [];
+            $data_user = [];
+            $data_mtr = [];
+            
+
+            foreach ($user as $us) {
+                $datas['id_user'] = $us->id;
+                $datas['nama_user'] = $us->nama;
+                $datas['email'] = $us->email;
+                array_push($data_user, $datas);
+            }
+            foreach ($kursus as $kur) {
+                $data['id_kursus'] = $kur->id;
+                $data['nama_instruktur'] = $kur->instruktur->nama;
+                $data['nama_kursus'] = $kur->judul_kursus;
+                $data['foto_instruktur'] = $kur->instruktur->foto;
+                $data['foto_kursus'] = $kur->foto;
+                $data['harga'] = $kur->harga_kursus;
+                $data['tipe_kursus'] = $kur->tipe_kursus;
+                array_push($data_kursus, $data);
+            }
+            foreach($kelas as $kel){
+                $kel->materi;
+            }
             return response()->json(
                 [
                     'success' => true,
                     'message' => 'history berhasil diambil',
                     'data' =>[
-                        'history'=>$history,
+                        'data_user'=>$data_user,
+                        'data_kursus'=>$data_kursus,
+                        'data_kelas'=>$kelas,
+                        'data_materi'=>$data_mtr,
                     ] 
                 ],
                 201
